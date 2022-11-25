1d8410090bc1d37b70e84f018666f032cee12464
Rafael Lemes <rafael.sslemes@gmail.com>
2022-11-25 13:37:03 -0300

    Initial implementation of Cloud Gateway

---
+++ ms-cloud-gateway/src/main/java/com/study/mscloudgateway/MsCloudGatewayApplication.java
@@ -0,0 +1,27 @@
+package com.study.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class MsCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder.routes()
+			.route(route -> route.path("/customers/**").uri("lb://ms-customers"))
+			.build();
+	}
+
+}
