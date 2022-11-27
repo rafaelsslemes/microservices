0e9a11d5db962b7a96ae74adb1e2a5652e283f11
Rafael Lemes <rafael.sslemes@gmail.com>
2022-11-27 08:27:36 -0300

    Added endpoint to get the cards of customer

--- ms-credit-card/src/main/java/com/study/mscreditcard/application/CreditCardController.java
+++ ms-credit-card/src/main/java/com/study/mscreditcard/application/CreditCardController.java
@@ -3,6 +3,7 @@ package com.study.mscreditcard.application;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.study.mscreditcard.domain.CreditCard;
-import com.study.mscreditcard.domain.representation.CreditCardSaveRequest;
+import com.study.mscreditcard.domain.CustomerCard;
+import com.study.mscreditcard.domain.representation.CreditCardDTO;
+import com.study.mscreditcard.domain.representation.CustomerCardDTO;
 
 @RestController
 @RequestMapping("cards")
 public class CreditCardController {
 
     @Autowired
-    private CreditCardService service;
+    private CreditCardService cardService;
+
+    @Autowired
+    private CustomerCardService customerCardService;
     
     @GetMapping
     public String getStatus(){
@@ -31,7 +37,7 @@ public class CreditCardController {
 
     @GetMapping(params = "id")
     public ResponseEntity<CreditCard> getCard(@RequestParam("id") Long id){
-        Optional <CreditCard> creditCard = service.findById(id);
+        Optional <CreditCard> creditCard = cardService.findById(id);
 
         if(creditCard.isPresent()){
             return ResponseEntity.ok().body(creditCard.get());
@@ -41,9 +47,9 @@ public class CreditCardController {
     }
 
     @PostMapping
-    public ResponseEntity register(@RequestBody CreditCardSaveRequest request) {
+    public ResponseEntity register(@RequestBody CreditCardDTO request) {
         var creditCard = request.toModel();
-        creditCard = service.save(creditCard);
+        creditCard = cardService.save(creditCard);
 
         URI headerLocation = ServletUriComponentsBuilder
             .fromCurrentRequest()
@@ -56,8 +62,16 @@ public class CreditCardController {
 
     @RequestMapping(params = "income")
     public ResponseEntity<List<CreditCard>> getCompatibleCards(@RequestParam("income") Long customerIncome) {
-        List<CreditCard> cards = service.getCompatibleCards(customerIncome);
+        List<CreditCard> cards = cardService.getCompatibleCards(customerIncome);
 
         return ResponseEntity.ok().body(cards);
+    }
+
+    @RequestMapping(params = "doc")
+    public ResponseEntity<List<CustomerCardDTO>> getCustomerCards(@RequestParam String doc) {
+        List<CustomerCard> cards = customerCardService.getCardByDoc(doc);
+        List<CustomerCardDTO> cardDTOs = cards.stream().map(CustomerCardDTO::fromModel).collect(Collectors.toList());   
+        
+        return ResponseEntity.ok().body(cardDTOs);
     }
 }
