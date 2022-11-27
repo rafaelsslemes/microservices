5fa3b259f127ccb3dfd95f663eddf7abe8f83432
Rafael Lemes <rafael.sslemes@gmail.com>
2022-11-27 19:10:38 -0300

    CreditAppraiser Service

---
+++ ms-credit-appraiser/src/main/java/com/study/mscreditappraiser/application/CreditAppraiserService.java
@@ -0,0 +1,13 @@
+package com.study.mscreditappraiser.application;
+
+import org.springframework.stereotype.Service;
+
+import com.study.mscreditappraiser.domain.CustomerState;
+
+@Service
+public class CreditAppraiserService {
+
+    public CustomerState getCustomerState(String doc) {
+        return null;
+    }
+}
