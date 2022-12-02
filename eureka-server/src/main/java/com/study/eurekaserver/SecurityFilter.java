80cf7296d16bcd7be48739066391e380b02f3eec
Rafael Lemes <rafael.sslemes@gmail.com>
2022-12-01 23:13:21 -0300

    Configuration to protect Eureka by BasicAuthentication

---
+++ eureka-server/src/main/java/com/study/eurekaserver/SecurityFilter.java
@@ -0,0 +1,21 @@
+package com.study.eurekaserver;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+public class SecurityFilter extends GenericFilterBean{
+    
+    @Override
+    public void doFilter(
+      ServletRequest request, 
+      ServletResponse response,
+      FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(request, response);
+    }
+}
